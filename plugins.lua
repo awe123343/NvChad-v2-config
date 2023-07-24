79b81b3477b7fcf5982f4af2c78147d29658b0d1
Sylvester Wang <awe1233430@hotmail.com>
2023-07-24 01:20:15 -0700

    fix(rust): only run crates on toml

--- plugins.lua
+++ plugins.lua
@@ -459,7 +459,7 @@ local plugins = {
   },
   {
     "saecki/crates.nvim",
-    ft = { "rust", "toml" },
+    ft = { "toml" },
     config = function(_, opts)
       local crates = require "crates"
       crates.setup(opts)
